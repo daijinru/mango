3d7f8211730a20febb3a8305796616b530209694
daijr <jeocat@163.com>
2023-01-19 18:09:44 +0800

    [console] rm test and upd package name

--- console/src/test/java/com/mango/console/ConsoleApplicationTests.java
+++ console/src/test/java/com/mango/console/ConsoleApplicationTests.java
@@ -6,17 +6,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConsoleApplicationTests {
 
-	@Test
-	void contextLoads() {
-		try {
-			Runtime rt = Runtime.getRuntime();
-			Process proc = rt.exec("notepad");
-			int exitVal = proc.waitFor();
-			System.out.println("Process exitValue: " + exitVal);
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-	}
+//	@Test
+//	void contextLoads() {
+//		try {
+//			Runtime rt = Runtime.getRuntime();
+//			Process proc = rt.exec("notepad");
+//			int exitVal = proc.waitFor();
+//			System.out.println("Process exitValue: " + exitVal);
+//		} catch (Throwable t) {
+//			t.printStackTrace();
+//		}
+//	}
 
 
 }
