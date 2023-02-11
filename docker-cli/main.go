8b19389eb764c8f7c8af6ed90ef5544809727a60
daijinru <jeocat@163.com>
2023-02-11 23:01:32 +0800

    [docker-cli] add commands

--- docker-cli/main.go
+++ docker-cli/main.go
@@ -13,6 +13,7 @@ func main() {
 			return nil
 		},
 	}
+
 	rootCommand.AddCommand(&command.Command{
 		Use: "images",
 		RunE: func(cmd *command.Command, args []string) error {
@@ -29,6 +30,7 @@ func main() {
 			return nil
 		},
 	})
+
 	rootCommand.AddCommand(&command.Command{
 		Use:  "image",
 		Args: command.ExactArgs(1),
@@ -36,5 +38,57 @@ func main() {
 			return nil
 		},
 	})
+
+	// get container status
+	rootCommand.AddCommand(&command.Command{
+		Use:  "container",
+		Args: command.ExactArgs(1),
+		RunE: func(cmd *command.Command, args []string) error {
+			return nil
+		},
+	})
+
+	rootCommand.AddCommand(&command.Command{
+		Use:  "get",
+		Args: command.ExactArgs(1),
+		RunE: func(cmd *command.Command, args []string) error {
+			return nil
+		},
+	})
+
+	rootCommand.AddCommand(&command.Command{
+		// @Todo support sub command
+		Use:  "run",
+		Args: command.ExactArgs(1),
+		RunE: func(cmd *command.Command, args []string) error {
+			return nil
+		},
+	})
+
+	// stop running container
+	rootCommand.AddCommand(&command.Command{
+		Use:  "stop",
+		Args: command.ExactArgs(1),
+		RunE: func(cmd *command.Command, args []string) error {
+			return nil
+		},
+	})
+
+	rootCommand.AddCommand(&command.Command{
+		Use:  "start",
+		Args: command.ExactArgs(1),
+		RunE: func(cmd *command.Command, args []string) error {
+			return nil
+		},
+	})
+
+	rootCommand.AddCommand(&command.Command{
+		Use:  "remove",
+		Args: command.ExactArgs(1),
+		RunE: func(cmd *command.Command, args []string) error {
+			return nil
+		},
+	})
+
 	rootCommand.Execute()
 }
