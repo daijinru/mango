4961651049cac2a036d95c8502a0c89d41e33b83
daijinru <jeocat@163.com>
2023-10-01 00:42:43 +0800

    :sparkles: [mango-cli] add ci running lock(file)

--- mango-cli/commands/ci.go
+++ mango-cli/commands/ci.go
@@ -1,28 +1,28 @@
 package cmd
 
 import (
-	"fmt"
-	command "github.com/daijinru/mango-packages-command"
-	"github.com/daijinru/mango/mango-cli/runner"
-	"github.com/daijinru/mango/mango-cli/utils"
+  "fmt"
+  command "github.com/daijinru/mango-packages-command"
+  "github.com/daijinru/mango/mango-cli/runner"
+  "github.com/daijinru/mango/mango-cli/utils"
 )
 
 func NewCmdCI() *command.Command {
-	root := &command.Command{
-		Use: "ci",
-		RunE: func(cmd *command.Command, args []string) error {
-			return nil
-		},
-	}
+  root := &command.Command{
+    Use: "ci",
+    RunE: func(cmd *command.Command, args []string) error {
+      return nil
+    },
+  }
   root.AddCommand(NewCmdGetConfig())
   root.AddCommand(NewCmdRunConfig())
-	return root
+  return root
 }
 
 func NewCmdGetConfig() *command.Command {
-	return &command.Command{
-		Use: "get",
-		RunE: func(cmd *command.Command, args []string) error {
+  return &command.Command{
+    Use: "get",
+    RunE: func(cmd *command.Command, args []string) error {
       var ci = &runner.CiConfig{}
       ci.NewCI()
       _, err := ci.ReadFromYaml(args[0])
@@ -33,19 +33,32 @@ func NewCmdGetConfig() *command.Command {
         fmt.Println(stage.Value)
       }
       return nil
-		},
-	}
+    },
+  }
 }
 
 func NewCmdRunConfig() *command.Command {
-	return &command.Command{
-		Use: "run",
-		RunE: func(cmd *command.Command, args []string) error {
-			ci := &runner.CiConfig{}
-			ci.NewCI()
-			_, err := ci.ReadFromYaml(args[0])
+  return &command.Command{
+    Use: "run",
+    RunE: func(cmd *command.Command, args []string) error {
+      lockName := ".ci.running"
+      workspace := &runner.WorkspaceClient{}
+      workspace.NewWorkSpaceClient(args[0])
+      
+      isLock, err := workspace.IfExistsLock(lockName)
+      utils.ReportErr(err)
+      if isLock {
+        fmt.Println("ci is running: " + lockName)
+        return nil
+      }
+
+      lockFile, err := workspace.CreateLockFile(lockName)
       utils.ReportErr(err)
 
+      ci := &runner.CiConfig{}
+      ci.NewCI()
+      _, err = ci.ReadFromYaml(args[0])
+      utils.ReportErr(err)
       runner.Setting(&runner.ExecutionOption{
         PrintLine: true,
       })
@@ -60,7 +73,10 @@ func NewCmdRunConfig() *command.Command {
           }
         }
       }
+      
+      fmt.Println("now release the lock: ", lockFile.Timestamp)
+      lockFile.DeleteLockFile()
       return nil
-		},
-	}
+    },
+  }
 }
--- mango-cli/runner/files.go
+++ mango-cli/runner/files.go
@@ -3,78 +3,136 @@ package runner
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/daijinru/mango/mango-cli/utils"
 )
 
 // It's the working directory client.
 type WorkspaceClient struct {
-	Workspace string `json:"Worksapce"`
+  Workspace string `json:"Worksapce"`
   Projects []string `json:"Projects"`
 }
 
-func (client *WorkspaceClient) ChWorkspace(path string) (string, error) {
-	err := os.Chdir(path)
-	if err != nil {
-		return "", err
-	}
+func (client *WorkspaceClient) chWorkspace(path string) (string, error) {
+  err := os.Chdir(path)
+  if err != nil {
+    return "", err
+  }
 
-	if (!client.PathExists(path)) {
+  if (!client.PathExists(path)) {
     return "", fmt.Errorf("path not exist: %s", path)
   }
 
-	dir, err := os.Getwd()
-	if err != nil {
-		return "", fmt.Errorf("no access: %s", path)
-	}
+  dir, err := os.Getwd()
+  if err != nil {
+    return "", fmt.Errorf("no access: %s", path)
+  }
 
-	return dir, nil
+  return dir, nil
 }
 
 func (client *WorkspaceClient) NewWorkSpaceClient(path string) (*WorkspaceClient, error) {
-	workspace, err := client.ChWorkspace(path)
-	utils.ReportErr(err)
+  workspace, err := client.chWorkspace(path)
+  utils.ReportErr(err)
   client.Workspace = workspace
-	return client, err
+  return client, err
 }
 
 func (client *WorkspaceClient) ListDirectories(path string) ([]string, error) {
-	var directories []string
+  var directories []string
 
-	files, err := os.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
+  files, err := os.ReadDir(path)
+  if err != nil {
+    return nil, err
+  }
 
-	for _, file := range files {
-		if file.IsDir() {
-			directories = append(directories, file.Name())
-		}
-	}
+  for _, file := range files {
+    if file.IsDir() {
+      directories = append(directories, file.Name())
+    }
+  }
 
-	return directories, nil
+  return directories, nil
 }
 
 func (client *WorkspaceClient) LsFiles(path string) ([]string, error) {
-	var out []string
+  var out []string
 
-	files, err := os.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
+  files, err := os.ReadDir(path)
+  if err != nil {
+    return nil, err
+  }
 
-	for _, file := range files {
-			out = append(out, file.Name())
-	}
+  for _, file := range files {
+      out = append(out, file.Name())
+  }
 
-	return out, err
+  return out, err
 }
 
 func (client *WorkspaceClient) PathExists(path string) bool {
-	_, err := os.Stat(path)
-	utils.ReportErr(err)
-	if (err != nil && os.IsNotExist(err)) {
-		return false
-	}
-	return true
+  _, err := os.Stat(path)
+  utils.ReportErr(err, "path not exists: %s")
+  if (err != nil && os.IsNotExist(err)) {
+    return false
+  }
+  return true
 }
+
+type LockFile struct {
+  Timestamp string
+  Name string
+  Exists bool
+}
+
+func (client *WorkspaceClient) IfExistsLock(name string) (bool, error) {
+  lockFilePath := name + ".lock" 
+  _, err := os.Stat(lockFilePath)
+  if err == nil {
+    return true, nil
+  } else if os.IsNotExist(err) {
+    return false, nil
+  } else {
+    return false, fmt.Errorf("unable to check file: %v", err)
+  }
+}
+
+func (client *WorkspaceClient) CreateLockFile(name string) (*LockFile, error) {
+  lockFile := &LockFile{
+    Name: name,
+    Timestamp: TimeNow(),
+    Exists: true,
+  }
+  lockFilePath := lockFile.Name + ".lock"
+  if _, err := os.Stat(lockFilePath); err == nil {
+    return lockFile, nil
+  } else if os.IsNotExist(err) {
+    file, err := os.Create(lockFilePath)
+    if err != nil {
+      return nil, fmt.Errorf("failed to create file: %v", err)
+    }
+    defer file.Close()
+    return lockFile, nil
+  } else {
+    return nil, fmt.Errorf("unable to check file: %v", err)
+  }
+}
+
+func (fileLock *LockFile) DeleteLockFile() error {
+  lockFilePath := fileLock.Name + ".lock"
+  if _, err := os.Stat(lockFilePath); err == nil {
+    err := os.Remove(lockFilePath)
+    if err != nil {
+      return fmt.Errorf("lock file cannot be deleted: %v", err)
+    }
+  }
+  fileLock.Exists = false
+  return nil
+}
+
+func TimeNow() string {
+  return time.Now().Format("01-02-2006 15:04:05")
+}
+
+
