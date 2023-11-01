10a8cd483e295cd52aa3501be32cd37bb9a0fe72
daijinru <jeocat@163.com>
2023-11-02 00:28:09 +0800

    :construction: [console] mysql connect

---
+++ console/src/main/java/com/mango/console/entities/Project.java
@@ -0,0 +1,21 @@
+package com.mango.console.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "path")
+    private String path;
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date createdAt;
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Date updatedAt;
+    // getters and setters
+}
