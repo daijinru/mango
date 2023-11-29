bd9750131305b815e95abeb6764624c2d9fe9166
daijinru <jeocat@163.com>
2023-11-29 22:44:50 +0800

    :fire: remove(console/deploy) rm Deploy entity

--- console/src/main/java/com/mango/console/services/dao/DeployRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mango.console.services.dao;
-
-import com.mango.console.services.entity.Deploy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeployRepo extends JpaRepository<Deploy, Long> {
-}
