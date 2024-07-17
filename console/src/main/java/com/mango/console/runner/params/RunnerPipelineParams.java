7fddbafb062fcae3056aefb76016c248a470901f
daijinru <jeocat@163.com>
2024-07-17 09:07:57 +0800

    [071601] :construction: (RunnerHttp) add RunnerPipelineParams

--- console/src/main/java/com/mango/console/runner/params/RunnerPipelineParams.java
+++ console/src/main/java/com/mango/console/runner/params/RunnerPipelineParams.java
@@ -1,4 +1,30 @@
 package com.mango.console.runner.params;
 
-public class RunnerPipelineParams {
+import lombok.Builder;
+import lombok.Data;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+public class RunnerPipelineParams extends RunnerBaseParams implements ParamsStrategy {
+    private String name;
+    /**
+     * the passing command text remotely, running in linux bash
+     */
+    private String command;
+    /**
+     * a text to store console print, which creating after pipeline executed by each application
+     */
+    private String filename;
+    public List returnParams() {
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("name", name));
+        params.add(new BasicNameValuePair("command", command));
+        params.add(new BasicNameValuePair("filename", filename));
+        return params;
+    }
 }
